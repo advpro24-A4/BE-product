be3d163be598e116a052e56fd6404ae8ba77e5db
vinamyrnauli <vinasllgn@gmail.com>
2024-05-07 17:28:34 +0700

    [REFACTOR] remove public modifiers from ProductServicee interface

--- src/main/java/id/ac/ui/cs/advprog/youkosoproduct/service/ProductService.java
+++ src/main/java/id/ac/ui/cs/advprog/youkosoproduct/service/ProductService.java
@@ -4,9 +4,9 @@ import id.ac.ui.cs.advprog.youkosoproduct.model.Product;
 import java.util.List;
 
 public interface ProductService {
-    public Product createProduct(Product product);
-    public Product updateProduct(String productId, Product updatedProduct);
-    public List <Product> findAllProduct();
-    public Product findProductById(String productId);
-    public Product deleteProduct(String productId);
+    Product createProduct(Product product);
+    Product updateProduct(String productId, Product updatedProduct);
+    List <Product> findAllProduct();
+    Product findProductById(String productId);
+    Product deleteProduct(String productId);
 }
