1b485e6aa0c9d489cc15c2802645c839266ba3aa
vinamyrnauli <vinasllgn@gmail.com>
2024-05-09 21:18:00 +0700

    [GREEN] Implement Product repository again

---
+++ src/main/java/id/ac/ui/cs/advprog/youkosoproduct/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package id.ac.ui.cs.advprog.youkosoproduct.repository;
+
+import id.ac.ui.cs.advprog.youkosoproduct.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+    Optional <Product> findByProductName(String productName);
+}
