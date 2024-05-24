62f634a92fcb6082b130890f561f25c6a1e45bef
vinamyrnauli <vinasllgn@gmail.com>
2024-05-24 23:25:23 +0700

    implement Order, OrderItem, Payment, Shipment model

---
+++ src/main/java/id/ac/ui/cs/advprog/youkosoproduct/model/OrderItem.java
@@ -0,0 +1,29 @@
+package id.ac.ui.cs.advprog.youkosoproduct.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "order_items")
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Column(name = "quantity", nullable = false)
+    private int quantity;
+
+    @Column(name = "price", nullable = false)
+    private double price;
+}
