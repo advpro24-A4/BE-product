7932fccfb14262a9956c66bf20c96475087f6cf1
vinamyrnauli <vinasllgn@gmail.com>
2024-05-25 21:58:54 +0700

    setup sonarqube

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
 	id("org.springframework.boot") version "3.2.5"
 	id("io.spring.dependency-management") version "1.1.4"
 	id ("org.flywaydb.flyway") version "10.11.0"
+	id("org.sonarqube") version "4.4.1.3373"
 	jacoco
 }
 
