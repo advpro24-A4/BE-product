ed5604abdb791d303443275715bddd7da226c0ef
Haekal Kalipaksi <hkalipaksi@proton.me>
2024-05-26 22:57:38 +0700

    fix finish request dto

--- src/main/java/id/ac/ui/cs/advprog/youkosoproduct/dto/FinishRequest.java
+++ src/main/java/id/ac/ui/cs/advprog/youkosoproduct/dto/FinishRequest.java
@@ -1,10 +1,13 @@
 package id.ac.ui.cs.advprog.youkosoproduct.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class FinishRequest {
+
+    @JsonProperty("order_id")
     private Long orderId;
 }
